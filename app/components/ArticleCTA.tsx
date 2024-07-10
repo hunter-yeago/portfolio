ce92f6e024fe77ea66b0261e881c49934e186c2a
hyradar <hyradar@tutamail.com>
2024-07-09 23:15:50 -0500

    progress to article cta

--- app/components/ArticleCTA.tsx
+++ app/components/ArticleCTA.tsx
@@ -4,16 +4,18 @@ import hunterImage from '../../public/images/hunter-at-phptek.png';
 export default function ProjectSection() {
 
 return(
-    <div className="w-4/5 mx-auto mt-12">
+    <div className="max-w-[70vw] mx-auto mt-12
+    md-custom:max-w-[60vw]">
     <h2 className='text-3xl text-center w-fit mx-auto mb-8'> PHP[TEK] 2024 </h2>
-    <div className=" mx-auto bg-white p-4 rounded shadow min-h-[20rem]">
+    <div className=" mx-auto bg-white p-4 rounded border-2 shadow min-h-[20rem]">
         
-        <div className="flex flex-row gap-4">
+        <div className="flex flex-col gap-4 w-full
+        md-custom:flex-row">
             
-            <div className="relative max-w-80">
-                <figure>
-                    <Image src={hunterImage} alt="Picture of me at PHP TEK 2024"/>
-                    <figcaption className="text-center"> Picture by Daryl</figcaption>
+            <div className="relative">
+                <figure className="relative object-cover h-full">
+                    <Image className="object-cover h-full" src={hunterImage} alt="Picture of me at PHP TEK 2024"/>
+                    <figcaption className="text-center absolute bottom-0 left-1 text-white"> Picture by Dwayne McDaniel</figcaption>
                 </figure>
                 
                 
@@ -21,9 +23,11 @@ return(
 
                 
             <div className="p-4 flex flex-col gap-4">
-                <p className="text-gray-700"> I went to PHP[TEK] 2024, and here's my article about it , and here's my article about it! , and here's my article about it! , and here's my article about it! , and here's my article about it! , and here's my article about it!!  </p>
-                <p className="text-gray-700"> I went to PHP[TEK] 2024, and here's my article about it , and here's my article about it! , and here's my article about it! , and here's my article about it! , and here's my article about it! , and here's my article about it!!  </p>
+                <p className="text-gray-700"> I went to PHP[TEK] 2024, and here's my article about it , and here's my article about it! , and here's my article about it! , and here's my article about it! </p>
+                <p className="text-gray-700"> I went to PHP[TEK] 2024, and here's my article about it , and here's my article about it! , and here's my article about it! , and here's my article about it!</p>
+                <a className='w-40 text-center bg-gray-800 text-white p-2 rounded-md hover:bg-yellow-600' href="https://www.linkedin.com/in/hunter-yeago/" target="_blank" rel="noopener noreferrer" aria-label="View my LinkedIn - opens in a new tab">Read Article</a>
             </div>
+
         </div>
 
     </div>
--- app/components/IntroParagraph.tsx
+++ app/components/IntroParagraph.tsx
@@ -57,7 +57,7 @@ const IntroParagraph: React.FC = () => {
                 <div className="flex flex-col justify-center gap-4 pr-4">
                     <h1 className='text-4xl font-semibold'>Hello! I&#39;m Hunter, a developer based in <span id="chicago">Chicago</span>.</h1>
                     <p> I love building tools that are <span id="user-friendly">user-friendly,</span> simple, and <span id="delightful">delightful.</span></p> 
-                    <p className="max-w-3xl"> I specialize in creating tools that are user-friendly, simple, and delightful. As a web developer at Lettuce Entertain You, Chicago's largest restaurant company, I manage and create new websites for over 70+ properties, ensuring each one is optimized for performance and user experience.</p>
+                    <p className="max-w-3xl"> I specialize in creating tools that are user-friendly, simple, and delightful. As a web developer at Lettuce Entertain You, Chicago's <span id="70websites">largest</span> restaurant company, I manage and create new websites for over 70+ properties, ensuring each one is optimized for performance and user experience.</p>
                     <p className="max-w-3xl"> Beyond my technical expertise, My strong communication skills allow me to effectively convey complex technical concepts to non-technical stakeholders, fostering a collaborative and productive working environment.</p>
                     <p> I love building tools that are user-friendly, simple, and delightful.</p> 
                     
@@ -65,8 +65,8 @@ const IntroParagraph: React.FC = () => {
                     <div className='flex justify-center gap-4 mt-4
                     md-custom:justify-start'>
                         <span id="linkedin" className='flex'>
-                            <a className='min-w-40 text-center bg-gray-800 text-white p-2 rounded-md hover:bg-yellow-600' href="https://www.linkedin.com/in/hunter-yeago/" target="_blank" rel="noopener noreferrer" aria-label="View my LinkedIn - opens in a new tab">View LinkedIn</a></span>
-                            <a className='min-w-40 text-center bg-white text-black border border-gray-800 p-2 rounded-md hover:bg-yellow-600' href="https://github.com/hyradar" target="_blank" rel="noopener noreferrer" aria-label="View my Github - opens in a new tab">View Github</a>
+                            <a className='w-40 text-center bg-gray-800 text-white p-2 rounded-md hover:bg-yellow-600' href="https://www.linkedin.com/in/hunter-yeago/" target="_blank" rel="noopener noreferrer" aria-label="View my LinkedIn - opens in a new tab">View LinkedIn</a></span>
+                            <a className='w-40 text-center bg-white text-black border border-gray-800 p-2 rounded-md hover:bg-yellow-600' href="https://github.com/hyradar" target="_blank" rel="noopener noreferrer" aria-label="View my Github - opens in a new tab">View Github</a>
                     </div>
                 </div>
 
