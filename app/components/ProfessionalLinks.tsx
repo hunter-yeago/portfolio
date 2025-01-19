0ab1d2e2180b502115f40219200a62d9f889c92b
hyradar <hyradar@tutamail.com>
2025-01-19 17:38:18 -0600

    added resume to website

--- app/components/ProfessionalLinks.tsx
+++ app/components/ProfessionalLinks.tsx
@@ -1,9 +1,9 @@
 export default function ProfessionalLinks() {
   return (
-    <div className="flex justify-center gap-4 mt-4 md-custom:justify-start">
+    <div className="flex flex-wrap justify-center gap-4 mt-4 md-custom:justify-start">
       <a
         id="linkedin"
-        className="md:w-40 text-center bg-gray-800 text-white p-2 rounded-md hover:bg-yellow-600"
+        className="w-40 text-center bg-gray-800 text-white p-2 rounded-md hover:bg-yellow-600"
         href="https://www.linkedin.com/in/hunter-yeago/"
         target="_blank"
         aria-label="View Hunter Yeagos LinkedIn - opens in a new tab"
@@ -12,7 +12,16 @@ export default function ProfessionalLinks() {
       </a>
 
       <a
-        className="md:w-40 text-center bg-white text-black border border-gray-800 p-2 rounded-md hover:bg-yellow-600"
+        className="w-40 text-center bg-[#7fbeeb] text-gold border border-gray-800 p-2 rounded-md hover:bg-yellow-600"
+        href="https://drive.google.com/file/d/1wwDmAvaOK3_vVJL6qeQAVk6Uegp8mUwv/view"
+        target="_blank"
+        aria-label="View Hunter Yeagos Resume - opens in a new tab"
+      >
+        View Resume
+      </a>
+
+      <a
+        className="w-40 text-center bg-white text-black border border-gray-800 p-2 rounded-md hover:bg-yellow-600"
         href="https://github.com/hunter-yeago"
         target="_blank"
         aria-label="View Hunter Yeagos Github - opens in a new tab"
