9d0a80176a38f4e1b23617621cc4b37b0fc6a024
hyradar <hyradar@tutamail.com>
2025-01-19 16:53:06 -0600

    fixed metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 "use client";
-import Image from "next/image";
 import Navbar from "../app/components/Navbar";
 import IntroParagraph from "../app/components/IntroParagraph";
-import ProjectSection from "../app/components/ProjectSection";
-import ArticleCTA from "../app/components/ArticleCTA";
-import Link from "next/link";
 
 export default function Home() {
   return (
