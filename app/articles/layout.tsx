95d64712a2e3e289036247c84d1c821fd8fc4fbb
hyradar <hyradar@tutamail.com>
2025-01-19 16:55:28 -0600

    added some more metadata

--- app/articles/layout.tsx
+++ app/articles/layout.tsx
@@ -3,6 +3,17 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Hunter Yeago",
   description: "Hunter Yeago's portfolio and personal website",
+  keywords: ["Hunter Yeago", "portfolio", "web developer", "personal website"],
+  authors: [{ name: "Hunter Yeago", url: "https://hunteryeago.com" }],
+  creator: "Hunter Yeago",
+  openGraph: {
+    title: "Hunter Yeago",
+    description: "Welcome to Hunter Yeago's portfolio and personal website.",
+    url: "https://hunteryeago.com",
+    siteName: "Hunter Yeago",
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
