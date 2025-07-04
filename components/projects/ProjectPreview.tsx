bd41a876493478373e896b6fbff57531fa6b2158
Hunter Yeago <hyradar@tutamail.com>
2025-07-04 10:50:51 -0500

    grid for project preview

--- components/projects/ProjectPreview.tsx
+++ components/projects/ProjectPreview.tsx
@@ -10,14 +10,13 @@ interface Props {
 export default function ProjectPreview({ project }: Props) {
   return (
     <Link
-      className="p-6 border-4 cursor-pointer focus:border-gray-800 focus:outline-none flex gap-14 justify-between rounded-xl shadow-lg hover:border-gray-800 hover:shadow-xl"
+      className="p-6 border-4 cursor-pointer focus:border-gray-800 focus:outline-none grid grid-cols-1 grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 gap-14 justify-between rounded-xl shadow-lg hover:border-gray-800 hover:shadow-xl"
       aria-label={`learn about my work on the ${project.title} project`}
       href={`/projects/${project.slug}`}
     >
       <div className="flex flex-col gap-2">
         <h2 className="text-xl font-bold">
-          {project.title} -
-          <span className="text-gray-500"> {project.type}</span>
+          {project.title} -<span className="text-gray-500"> {project.type}</span>
         </h2>
 
         <div className="flex flex-col gap-1">
@@ -29,14 +28,8 @@ export default function ProjectPreview({ project }: Props) {
         <TechStackList items={project.tech_stack} useLinks={false} />
       </div>
 
-      <div className="relative min-w-[200px] min-h-[200px] overflow-hidden">
-        <Image
-          className="absolute w-full h-full object-cover"
-          src={project.preview.image}
-          alt={`${project.title} preview image`}
-          width={300}
-          height={300}
-        />
+      <div className="relative overflow-hidden">
+        <Image className="absolute w-full h-full object-cover" src={project.preview.image} alt={`${project.title} preview image`} width={300} height={300} />
       </div>
     </Link>
   );
