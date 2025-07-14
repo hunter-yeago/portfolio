27112b9404d18dc6da9649766ec4a27ac7711155
Hunter Yeago <hyradar@tutamail.com>
2025-07-14 08:53:37 -0500

    commented out navlinks for now

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,7 +5,7 @@ import NavLinks from "../nav/Navlinks";
 export default function Footer() {
   return (
     <footer className="flex flex-col gap-4 min-h-40 border-t mt-8 p-8">
-      <NavLinks />
+      {/* <NavLinks /> */}
 
       <ul className="ml-auto w-fit flex gap-2">
         <li>
