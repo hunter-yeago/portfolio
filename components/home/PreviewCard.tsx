04e8ec5b38f60a7e82534038dce551775a4a082f
hunter <hunter@hunters-MacBook-Pro.local>
2025-08-29 12:32:58 -0500

    small cleanup

--- components/home/PreviewCard.tsx
+++ components/home/PreviewCard.tsx
@@ -18,6 +18,10 @@ export default function PreviewCard({ project }: Props) {
       aria-label={`learn about my work on the ${project.title} project`}
       href={`/projects/${project.slug}`}
     >
+      <h2 className="text-xl font-bold">
+          {project.title} -
+          <span className="text-gray-500"> {project.type}</span>
+        </h2>
       {/* Preview Image */}
       <div className="hidden md:block relative aspect-[16/9] overflow-hidden rounded-md">
         <Image
@@ -31,10 +35,7 @@ export default function PreviewCard({ project }: Props) {
 
       {/* Text + Tech Stack */}
       <div className="flex flex-col gap-2">
-        <h2 className="text-xl font-bold">
-          {project.title} -
-          <span className="text-gray-500"> {project.type}</span>
-        </h2>
+        
 
         <div className="flex flex-col gap-1 text-gray-700">
           {project.preview.description.map((paragraph, index) => (
